9b87f96cfa31fee83418d039bbad1b4b5be9b00f
Almanac <ruoyw0@gmail.com>
2021-04-16 11:51:11 -0700

    Partially filled in main.py game class. Starting to look into player classes

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import os
+from gesture import Gesture
 class Game:
 	def __init__(self):
+		self.reset()
+	def run(self):
 		print("hello world")
-		
-		
+	def reset(self):
+		os.system('cls')
+		self.welcome()
+		self.vsNPC = self.selectMode()
+	def selectMode(self):
+		value = None
+		while (value != "1" and value != "2") or value is None:
+			value = input("Please enter 1 for vs npc, or 2 for player vs player")
+		self.vsNPC = value == "1"
+		self.player1 = HumanPlayer()
+		if self.vsNPC:
+			
+	def welcome(self):
+		print("Welcome to Rock Paper Scissors Lizard Spock!")
+		print("Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock")
+		print("Rock rushes Lizard, Lizard poisons Spock, Spock smashes Scissors")
+		print("Scissors decapitates Lizard, Lizard eats paper, Paper disproves Spock,")
+		print("and Spock vaporizes rock")
+		print()
 if __name__ == "__main__":
 	game = Game();
---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+	def __init__(num):
+		self.player_num = num
+		self.moves = ["rock", "paper", "scissors", "lizard", "spock"]
+	
+	def takeAction():
+		raise NotImplementedError("Please implement")
